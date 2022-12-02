f3d99b5d2454f25b652bb78727588a5a12a8fddd
Grzegorz Kanclerz <grzegorz.kanclerz2@gmail.com>
2022-12-02 11:17:53 +0100

    init project

---
+++ src/app/layouts/fullpageadmin/fullpageadmin.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FullpageadminComponent } from './fullpageadmin.component';
+import { AdminComponent } from 'src/app/modules/admin/admin.component';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from 'src/app/shared/material.module';
+import { AdminProductComponent } from 'src/app/modules/admin/admin-product/admin-product.component';
+import { AdminProductUpdateComponent } from 'src/app/modules/admin/admin-product-update/admin-product-update.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AdminProductAddComponent } from 'src/app/modules/admin/admin-product-add/admin-product-add.component';
+import { AdminProductFormComponent } from 'src/app/modules/admin/admin-product-form/admin-product-form.component';
+
+@NgModule({
+  declarations: [
+    FullpageadminComponent,
+    AdminComponent,
+    AdminProductComponent,
+    AdminProductUpdateComponent,
+    AdminProductAddComponent,
+    AdminProductFormComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ]
+})
+export class FullpageadminModule { }
