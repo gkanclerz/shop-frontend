0eb317eaa03530a4f76eb3a9c8da744d4be53f40
Grzegorz Kanclerz <grzegorz.kanclerz2@gmail.com>
2022-12-07 13:56:36 +0100

    Obsługa kategorii w formularzu produktów

--- src/app/modules/admin/admin-product-form/admin-product-form.component.ts
+++ src/app/modules/admin/admin-product-form/admin-product-form.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { FormGroup } from "@angular/forms";
+import { AdminCategoryNameDto } from "./adminCategoryNameDto";
+import { FormCategoryService } from "./form-category.service";
 
 @Component({
-    selector: 'app-admin-product-form',
-    template: `
+  selector: 'app-admin-product-form',
+  template: `
     <div [formGroup]="parentForm" fxLayout="column"> 
     <mat-form-field appearance="fill">
     <mat-label>Nazwa</mat-label>
@@ -44,23 +46,19 @@ import { FormGroup } from "@angular/forms";
     </div>
     </mat-form-field>
 
-    <mat-form-field appearance="fill">
-      <mat-label>Pełny opis produktu (opcjonalnie)</mat-label>
-      <textarea matInput rows="20" placeholder="Podaj pełny opis produktu" formControlName="fullDescription"></textarea>
-    </mat-form-field>
-
-    <mat-form-field appearance="fill">
+      <mat-form-field appearance="fill">
       <mat-label>Kategoria</mat-label>
-      <input matInput placeholder="Podaj kategorię produktu" formControlName="category">
-      <div *ngIf="category?.invalid && (category?.dirty || category?.touched)" class="errorMessages">
-        <div *ngIf="category?.errors?.['required']">
+      <mat-select formControlName="categoryId">
+        <mat-option *ngFor="let el of categories" [value]="el.id">
+        {{el.name}}
+        </mat-option>
+      </mat-select>
+      <div *ngIf="categoryId?.invalid && (categoryId?.dirty || categoryId?.touched)" class="errorMessages">
+        <div *ngIf="categoryId?.errors?.['required']">
           Kategoria jest wymagana
         </div>
-        <div *ngIf="category?.errors?.['minlength']">
-          Kategoria musi mieć przynajmniej 4 znaki
-        </div>
     </div>
-    </mat-form-field>
+</mat-form-field>
 
     <mat-form-field appearance="fill">
       <mat-label>Cena</mat-label>
@@ -89,49 +87,58 @@ import { FormGroup } from "@angular/forms";
       <button mat-flat-button color="primary" [disabled]="!parentForm.valid">Zapisz</button>
     </div>
     </div>
-    `, styles:[`
+    `, styles: [`
     .errorMessages{
       color: red; 
       margin-top: 10px;
     }
     `]
 })
-export class AdminProductFormComponent implements OnInit{
+export class AdminProductFormComponent implements OnInit {
 
-    @Input() parentForm! : FormGroup; 
-    ngOnInit(): void {
+  @Input() parentForm!: FormGroup;
+  categories : Array<AdminCategoryNameDto> = []
 
-    }
+  constructor(private formCategoryService : FormCategoryService){}
 
-    get name(){
-      return this.parentForm.get("name");
-    }
+  ngOnInit(): void {
+    this.getCategories();
+  }
 
-    get description(){
-      return this.parentForm.get("description");
-    }
+  getCategories(){
+    this.formCategoryService.getCategories()
+    .subscribe(categories => this.categories = categories)
+  }
 
-    get fullDescription(){
-      return this.parentForm.get("fullDescription");
-    }
+  get name() {
+    return this.parentForm.get("name");
+  }
 
-    get category(){
-      return this.parentForm.get("category");
-    }
+  get description() {
+    return this.parentForm.get("description");
+  }
 
-    get price(){
-      return this.parentForm.get("price");
-    }
+  get fullDescription() {
+    return this.parentForm.get("fullDescription");
+  }
 
-    get currency(){
-      return this.parentForm.get("currency");
-    }
+  get categoryId() {
+    return this.parentForm.get("categoryId");
+  }
 
-    get slug(){
-      return this.parentForm.get("slug");
-    }
+  get price() {
+    return this.parentForm.get("price");
+  }
 
-    
+  get currency() {
+    return this.parentForm.get("currency");
+  }
+
+  get slug() {
+    return this.parentForm.get("slug");
+  }
+
+
 
 
 }
