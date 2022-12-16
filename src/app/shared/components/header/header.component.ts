f6d914c445a750c7b007dcb0f4224965b83c0a9f
Grzegorz Kanclerz <grzegorz.kanclerz2@gmail.com>
2022-12-16 12:53:20 +0100

    Ulepszenie ikonki koszyka

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { CartIconService } from 'src/app/modules/common/service/cart-icon.service';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +11,22 @@ import { CookieService } from 'ngx-cookie-service';
 export class HeaderComponent implements OnInit {
 
   title = "Shop";
-  cartId = Number(this.cookieService.get("cartId"));
-  constructor(private cookieService : CookieService) { }
+  cartProductCounter = "";
+  constructor(
+    private cookieService : CookieService,
+    private headerService : HeaderService,
+    private cartIconService: CartIconService
+    ) { }
 
   ngOnInit(): void {
+    this.getCountProducts();
+    this.cartIconService.subject
+    .subscribe(cartProductCounter => this.cartProductCounter = String(cartProductCounter > 0 ? cartProductCounter:""))
   }
 
+  getCountProducts(){
+    let cartId = Number(this.cookieService.get("cartId"));
+    this.headerService.getCountProducts(cartId)
+    .subscribe(cartProductCounter => this.cartProductCounter = String(cartProductCounter > 0 ? cartProductCounter:""))
+  }
 }
