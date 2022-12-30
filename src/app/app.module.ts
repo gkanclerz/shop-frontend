29a5212385be512ae111d6fafff77abc4576d46a
Grzegorz Kanclerz <grzegorz.kanclerz2@gmail.com>
2022-12-30 14:58:50 +0100

    Zabezpieczenie panelu administracyjnego

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultModule } from './layouts/default/default.module';
 import { FullpageModule } from './layouts/fullpage/fullpage.module';
-import { FullpageadminModule } from './layouts/fullpageadmin/fullpageadmin.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
+import { FullpageadminModule } from './layouts/fullpageadmin/fullpageadmin.module';
+import { FullpageadminemptyModule } from './layouts/fullpageadminempty/fullpageadminempty.module';
+import { JwtInterceptor } from './modules/common/interceptor/jwt.interceptor';
+import { AdminAuthorizeGuard } from './modules/admin/common/guard/adminAuthorizeGuard';
 
 @NgModule({
   declarations: [
@@ -20,10 +23,15 @@ import { CookieService } from 'ngx-cookie-service';
     DefaultModule,
     FullpageModule,
     FullpageadminModule,
+    FullpageadminemptyModule,
     BrowserAnimationsModule,
     HttpClientModule
   ],
-  providers: [CookieService],
+  providers: [
+    CookieService,
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    AdminAuthorizeGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
