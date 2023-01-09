cac2e0eb243d3916d7db293793db33a23ea0db73
Grzegorz Kanclerz <grzegorz.kanclerz2@gmail.com>
2023-01-09 14:29:08 +0100

    Strona główna

---
+++ src/app/modules/home/home.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HomePageDto } from './model/homePageDto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  constructor(private http : HttpClient) { }
+
+  getHomePageData() : Observable<HomePageDto>{
+    return this.http.get<HomePageDto>("/api/homePage")
+  }
+}
